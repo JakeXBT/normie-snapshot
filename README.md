d7329b8096da18be087a1756faec9b67d2d79835
gradientivx <99917971+JakeXBT@users.noreply.github.com>
2024-05-26 19:28:24 +0400

    Code clean-up, commentary and caching

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 The purpose of this repository is to check the balances of holders of the Normie token on Base prior to an exploit that occurred on 26th May 2024.
 
-The script `main.py` does data fetching and outputs to `holder_snapshot.json` a file which contains the balances of addresses who have interracted with the Normie token prior to the snapshot block.
+The script `main.py` does data fetching and outputs to `output/snapshot.json` a file which contains the balances of addresses who have interracted with the Normie token prior to the snapshot block. The code in `multicall.py` contains rug.ai's implementation of a client for optimized RPC batch calls.
 
-The code in `multicall.py` contains rug.ai's implementation of a client for optimized RPC calls.
+The `/files` folder contains contract ABIs for `Multicall` and the standard ERC-20 token implementation. The `/output` folder contains two output files which are written by the `main.py` script:
 
-The `/files` folder contains contract ABIs for `Multicall` and the standard ERC-20 token implementation.
+- `transfers.json` is a list of all token transfers which occurred prior to the exploit snapshot;
+- `snapshot.json` is a list of all token holders and the amount of tokens they held at the snapshot block.
 
 ## Running a Local Fork of Base
 
@@ -15,5 +16,5 @@ In order to run this script, you will need to fork the Base network at a specifi
 Anvil provides a way of achieving this:
 
 ```
-anvil -f https://base.rpc.rug.ai/[API KEY]--fork-block-number 14949166 --port 8545
+anvil -f https://base.rpc.rug.ai/[API KEY] --fork-block-number 14949166 --port 8545
 ```
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from web3 import Web3, HTTPProvider
+"""
+This script contains the main logic for creating a snapshot of token holders at a specific block number.
+
+The script performs the following steps:
+1. Fetch all Transfer events for a specific token contract to identify a list of candidate holders to check.
+2. Fetch the list of all addresses involved in the transfers.
+3. Fetch the balances of all addresses in a single multicall.
+4. Exports the snapshot to a JSON file.
+"""
+
+from web3 import Web3
 import json
 
 from multicall import Multicall
+
+########################################################
+#                                                      #
+#                    Configuration                     #
+#                                                      #
+########################################################
 
 # Constants for analysis
 NORMIE_TOKEN_ADDRESS = "0x7F12d13B34F5F4f0a9449c16Bcd42f0da47AF200"
@@ -10,8 +26,7 @@ with open("files/ERC20.json", "r") as f:
     ERC20_ABI = json.load(f)
 
 # Create a Web3 instance pointing at the local Anvil fork of Base
-# w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-w3 = Web3(HTTPProvider("https://base.llamarpc.com"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 NORMIE_TOKEN = w3.eth.contract(address=NORMIE_TOKEN_ADDRESS, abi=ERC20_ABI)
 NORMIE_DECIMALS = NORMIE_TOKEN.functions.decimals().call()
@@ -22,7 +37,18 @@ multicall = Multicall(w3, "base")
 
 current_block = w3.eth.block_number
 
-# Step 1: Find all transfers of the token to identify a list of candidate holders to check
+# SAFETY CHECK: Ensure that the local Anvil fork is at the correct block number
+assert (
+    current_block == 14949166
+), "Please ensure that the local Anvil fork is at block 14949166."
+
+########################################################
+#                                                      #
+#                        Step 1                        #
+#                                                      #
+########################################################
+
+# STEP 1: Find all transfers of the token to identify a list of candidate holders to check
 transfers = []
 
 batch_size = 10000  # Number of blocks to fetch in each batch
@@ -39,7 +65,10 @@ def fetch_transfer_events(start_block, end_block):
         return []
 
 
-while current_block > w3.eth.block_number - 10000:
+# Keep a log of how many cycles have passed to ensure caching
+cycles = 0
+
+while current_block > 11486835:
     from_block = max(0, current_block - batch_size)
     to_block = current_block
     pool_created_events = fetch_transfer_events(from_block, to_block)
@@ -54,15 +83,27 @@ while current_block > w3.eth.block_number - 10000:
             }
             transfers.append(transfer_data)
         except Exception as e:
+            print(f"Error processing event: {e}")
             pass  # Ignore errors and continue
 
     print(f"Processed blocks from {from_block} to {to_block}")
     current_block = from_block - 1
 
+    cycles += 1
+
+    if cycles % 10 == 0:
+        print(f"Saving transfers to a JSON file...")
+        with open("output/transfers.json", "w") as f:
+            json.dump(transfers, f)
+
+# Save the transfers to a JSON file
+with open("output/transfers.json", "w") as f:
+    json.dump(transfers, f)
+
+
 print(f"Total number of transfers: {len(transfers)}")
 
-
-# Step 2: Fetch the lis of all addresses involved in the transfers
+# STEP 2: Fetch the lis of all addresses involved in the transfers
 addresses = set()
 
 for transfer in transfers:
@@ -71,36 +112,48 @@ for transfer in transfers:
 
 addresses = list(addresses)
 
-# Step 3: Fetch the balances of all addresses in a single multicall
-# TODO: Batch these calls to avoid hitting the gas limit
-calls = [
-    multicall.create_call(
-        NORMIE_TOKEN,
-        "balanceOf",
-        args={"_owner": w3.to_checksum_address(address)},
-    )
-    for address in addresses
+print(f"Total number of addresses: {len(addresses)}")
+
+# STEP 3: Fetch the balances of all addresses in a single multicall
+address_balances = []
+
+# Batch addresses into groups of 1000 to avoid hitting the gas limit
+batch_size = 250
+address_batches = [
+    addresses[i : i + batch_size] for i in range(0, len(addresses), batch_size)
 ]
 
-balances = multicall.call(calls)
+print(f"Total number of batches: {len(address_batches)}")
 
-# Reformat the balances
-balances = [
-    max(int.from_bytes(item[1], "big") / NORMIE_RESCALING_FACTOR, 0)
-    if item[0]
-    else None
-    for item in balances
-]
+# Fetch the balances for each batch of addresses
+for batch in address_batches:
+    calls = [
+        multicall.create_call(
+            NORMIE_TOKEN,
+            "balanceOf",
+            args={"_owner": w3.to_checksum_address(address)},
+        )
+        for address in batch
+    ]
 
-print(f"Fetched balances for {len(balances)} addresses")
-print(f"Number of calls made: {len(calls)}")
+    balances = multicall.call(calls)
 
-# Step 4: Combine the balances with the addresses
-address_balances = [
-    {"address": address, "balance": balance}
-    for address, balance in zip(addresses, balances)
-]
+    # Reformat the balances
+    balances = [
+        max(int.from_bytes(item[1], "big") / NORMIE_RESCALING_FACTOR, 0)
+        if item[0]
+        else None
+        for item in balances
+    ]
 
-# Step 5: Export to a JSON file
-with open("holder_snapshot.json", "w") as f:
+    # Combine the balances with the addresses
+    address_balances += [
+        {"address": address, "balance": balance}
+        for address, balance in zip(batch, balances)
+    ]
+
+    print(f"Processed batch of {len(batch)} addresses")
+
+# STEP 4: Export to a JSON file
+with open("output/snapshot.json", "w") as f:
     json.dump(address_balances, f)
